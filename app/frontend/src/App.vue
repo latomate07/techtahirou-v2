1d5e509c7f1ac18ef394b3be549b34c1a6efb33d
Tahirou Magagi <65642962+latomate07@users.noreply.github.com>
2025-05-02 19:33:00 +0200

    fix(App.vue): update email sender logic and improve link accessibility in contact section

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -128,7 +128,6 @@ const sendEmail = async (e) => {
             },
             body: JSON.stringify({
                 to: API_EMAIL_RECEIVER,
-                from: API_EMAIL_RECEIVER,
                 subject: 'techTahirou - Nouveau message de contact',
                 text: `Name: ${contactForm.name}\nEmail: ${contactForm.email}\nSubject: ${contactForm.subject}\nMessage: ${contactForm.message}`,
                 html: `
@@ -330,7 +329,7 @@ onMounted(() => {
                             </svg>
                         </a>
                         <div class="flex px-3 py-4 items-center space-x-6">
-                            <a :href="contactInfos && contactInfos?.length > 0 ? contactInfos[0].github : '#'"
+                            <a :href="contactInfos && contactInfos?.length > 0 ? contactInfos[0].github : '#'" target="_blank"
                                 @mouseenter="isHoveringInteractive = true" @mouseleave="isHoveringInteractive = false"
                                 class="text-gray-400 hover:text-indigo-400 transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
@@ -339,7 +338,7 @@ onMounted(() => {
                                         d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                                 </svg>
                             </a>
-                            <a :href="contactInfos && contactInfos?.length > 0 ? contactInfos[0].linkedin : '#'"
+                            <a :href="contactInfos && contactInfos?.length > 0 ? contactInfos[0].linkedin : '#'" target="_blank"
                                 @mouseenter="isHoveringInteractive = true" @mouseleave="isHoveringInteractive = false"
                                 class="text-gray-400 hover:text-indigo-400 transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
@@ -366,7 +365,7 @@ onMounted(() => {
                             </span>
                         </div>
 
-                        <h1 class="text-5xl sm:text-6xl lg:text-7xl font-bold tracking-tight leading-tight mb-8">
+                        <h1 class="text-[2rem] sm:text-5xl lg:text-7xl font-bold tracking-tight leading-tight mb-8">
                             <span class="block relative overflow-hidden">
                                 <span class="block transform transition-transform">
                                     <span
@@ -376,7 +375,7 @@ onMounted(() => {
                             <span class="block">Logiciel Web/Mobile</span>
                         </h1>
 
-                        <p class="text-white/70 text-lg sm:text-xl leading-relaxed mb-8 max-w-xl">
+                        <p class="text-white/70 text-md sm:text-xl leading-relaxed mb-8 max-w-xl">
                             Je conçois des expériences numériques de pointe qui allient une esthétique époustouflante à
                             une fonctionnalité sans faille.
                         </p>
@@ -583,7 +582,7 @@ onMounted(() => {
                     <!-- button see more -->
                     <div v-if="visibleCount < projects.length" class="flex justify-center mt-6">
                         <button @click="loadMore"
-                                class="bg-white/5 w-max mx-auto backdrop-blur-md text-white px-6 py-3 rounded-full hover:bg-white/20 transition-colors duration-200 text-sm font-medium flex items-center justify-center">
+                                class="bg-white/5 w-max cursor-pointer mx-auto backdrop-blur-md text-white px-6 py-3 rounded-full hover:bg-white/20 transition-colors duration-200 text-sm font-medium flex items-center justify-center">
                                 Voir plus de projets
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-2" fill="none"
                                 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -672,7 +671,7 @@ onMounted(() => {
                                     <span v-if="!selectedProject.liveLink || selectedProject.liveLink == null || selectedProject.liveLink == '#'"
                                         class="text-xs mt-1">
                                         <span class="text-gray-400">
-                                            Lien inaccessible.
+                                            Lien non disponible.
                                         </span>
                                     </span>
                                 </div>
@@ -695,7 +694,7 @@ onMounted(() => {
                                     <span v-if="!selectedProject.repoLink || selectedProject.repoLink == null || selectedProject.repoLink == '#'"
                                         class="text-xs mt-1">
                                         <span class="text-gray-400">
-                                            Lien inaccessible.
+                                            Lien non disponible.
                                         </span>
                                     </span>
                                 </div>
